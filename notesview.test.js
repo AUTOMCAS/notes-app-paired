cf977cb1b947ffb44ad04c961d09ed9e12c7a20e
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-25 12:57:47 +0100

    Modify test

--- notesview.test.js
+++ notesview.test.js
@@ -3,22 +3,24 @@
  */
 
 const fs = require('fs');
+
+const NotesModel = require('./notesModel');
 const NotesView = require('./notesView');
 
-describe('Page view', () => {
-  it('displays the title', () => {
+describe('Notes view', () => {
+  it('displays two notes', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
 
-    const notesView = new NotesView();
-    expect(document.querySelectorAll('h1').length).toBe(1);
-  });
-});
+    // 1. Setting up model and view
+    const model = new NotesModel();
+    const view = new NotesView(model);
+    model.addNote('A first note');
+    model.addNote('Another one');
 
-describe('displayNotes', () => {
-  it('displays the notes', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
-    const notesView = new NotesView();
+    // 2. Display the notes on the page
+    view.displayNotes();
 
-    expect(notesView.displayNotes()).toEqual(null);
+    // 3. There should now be 2 div.note on the page
+    expect(document.querySelectorAll('div.note').length).toEqual(2);
   });
 });
