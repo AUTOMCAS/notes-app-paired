a5c8ea4a36f4b3e65e18aea62f767529b6acc6fa
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-25 10:07:24 +0100

    Install node Dand jest again

--- notesModel.js
+++ notesModel.js
@@ -1,3 +1,15 @@
-class NotesModel {}
+class NotesModel {
+  constructor() {
+    this.notes = [];
+  }
+
+  getNotes() {
+    return this.notes;
+  }
+
+  addNote(note) {
+    this.notes.push(note);
+  }
+}
 
 module.exports = NotesModel;
--- notesModel.test.js
+++ notesModel.test.js
@@ -1,6 +1,14 @@
 const NotesModel = require('./notesModel');
 
-it('returns an empty list of items', () => {
-  const notesModel = new NotesModel();
-  expect(notesModel.getNotes()).toEqual([]);
+describe('NotesModel', () => {
+  it('returns an empty list of items', () => {
+    const notesModel = new NotesModel();
+    expect(notesModel.getNotes()).toEqual([]);
+  });
+
+  it('adds a note to the notes array and returns it', () => {
+    const notesModel = new NotesModel();
+    notesModel.addNote('Pet the cat');
+    expect(notesModel.getNotes()).toEqual(['Pet the cat']);
+  });
 });
