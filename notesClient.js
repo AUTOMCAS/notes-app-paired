790f0be726a33f354000a45cccd63842a1ed3334
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-28 11:52:29 +0100

    Modify class to pass test createNote()

--- notesClient.js
+++ notesClient.js
@@ -6,6 +6,43 @@ class NotesClient {
         callback(data);
       });
   }
+
+  createNote(newNote, callback, displayError) {
+    fetch('http://localhost:3000/notes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newNote),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        callback(data);
+      })
+      .catch((error) => {
+        console.log('error');
+        displayError(error);
+      });
+  }
 }
 
 module.exports = NotesClient;
+
+// createNote(newNoteFromAPI, cb, displayError) {
+//   const content = {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ content: newNoteFromAPI })
+//   }
+//   fetch(this.#URL, content)
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log("Success:",data)
+//       cb(data)
+//     })
+//     .catch(error => {
+//       console.log('error')
+//       displayError(error)
+//   });
+// }
